bd4719abd51b49216f8bd750f50d5cd46dcfaa86
Max <38158634+maxdraz@users.noreply.github.com>
2019-03-14 22:49:44 +0000

    Made new audio reactive scene called "Max Audio Analyzer"

---
+++ Bull Smash Unity Project/Assets/AudioAnalyzer.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioAnalyzer : MonoBehaviour
+{
+    //mic vars
+    public string mic = "assigned at runtime";
+    //audio analysing vars
+    private float[] samples;
+    private int FRAME_SIZE = 1024; //gets sound level for the last 21ms (1024/48000)
+    private float sampleRate; //how many samples to take each sec
+    public float dbValue;
+    private AudioSource audio;
+    public float refValue = 0.0001f; //default: 0.1f, adjust if we need different 0db reference, 0.0001f return 0 most time
+
+
+    private void Start()
+    {
+
+        mic = null;
+
+        foreach(string device in Microphone.devices)
+        {
+            if(mic== null)
+            {
+                mic = device;
+            }
+
+        }
+        samples = new float[FRAME_SIZE];
+        sampleRate = AudioSettings.outputSampleRate;
+        audio = GetComponent<AudioSource>();
+
+        UpdateMicrophone();
+    }
+
+    private void Update()
+    {
+        AnalyzeAudio();
+    }
+
+    void UpdateMicrophone()
+    {
+        //do tomorrow
+        audio.Stop();
+        //start recording audio from mic
+        audio.clip = Microphone.Start(mic, true, 10, (int)sampleRate);
+        audio.loop = true;
+
+        if (Microphone.IsRecording(mic)) //avoids infinite loop
+        {
+            while(!(Microphone.GetPosition(mic) > 0))
+            {                
+                //start recording
+                audio.Play();
+                print("started recording");
+            }
+        }
+        else
+        {
+            //mic doesnt work for some reason
+            print("mic don't work");
+        }
+
+    }
+
+    void AnalyzeAudio()
+    {
+        //find RMS - sum all squared sample values, calculate average and get it's squared root
+        //convert RMS to dB using log operation
+
+        //get sound data and store amplitudes in the samples array
+        audio.GetOutputData(samples, 0);
+
+        float sum = 0;
+
+        for (int i = 0; i < FRAME_SIZE; i++)
+        {
+            sum += samples[i] * samples[i];
+        }
+
+        float rmsValue = Mathf.Sqrt(sum / FRAME_SIZE);
+        dbValue = 20 * Mathf.Log10(rmsValue / refValue);
+       // dbValue = Mathf.Clamp(dbValue, -40, 4);
+
+    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    //Gizmos.color = (dbValue >= -10) ? Color.red : Color.yellow;
+    //    //Gizmos.DrawSphere(transform.position, 5f);
+    //}
+}
---
+++ Bull Smash Unity Project/Assets/PlayerMove.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMove : MonoBehaviour
+{
+    public float anglesPerSec;
+    public float db;
+    public float minDb;
+    public float speed;
+    private Rigidbody rb;
+    public float boostCD= 1f;
+    public float boostCDRemaining;
+    public bool canBoost;
+
+    public GameObject soundCircle;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        db = GetComponent<AudioAnalyzer>().dbValue;
+        db = Mathf.Clamp(db, 0, 100f);
+        boostCDRemaining -= Time.deltaTime;
+        Rotate();
+        soundCircle.transform.localScale = new Vector3(0.5f*db,0.5f* db,0.5f * db);
+        if (boostCDRemaining <= 0)
+        {
+            boostCDRemaining = 0;
+            canBoost = true;           
+            
+        }
+
+        if (canBoost && db > minDb)
+        {
+            Boost();
+            boostCDRemaining = boostCD;
+            canBoost = false;
+        }
+    }
+
+    void Rotate()
+    {
+        float rotSpeed = anglesPerSec * Time.deltaTime;
+        Vector3 rotation = new Vector3(0, 1 * rotSpeed, 0);
+        transform.Rotate(rotation, Space.World);
+    }
+
+    void Boost()
+    {
+       rb.AddForce(transform.forward * db * speed * Time.deltaTime,ForceMode.Impulse);
+              
+    }
+}
