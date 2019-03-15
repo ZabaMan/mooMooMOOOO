a371dbfe5d2118a9b23aa527c9cb28fa918b85a4
Max <38158634+maxdraz@users.noreply.github.com>
2019-03-15 19:12:51 +0000

    changed force mode

--- Bull Smash Unity Project/Assets/PlayerMove.cs
+++ Bull Smash Unity Project/Assets/PlayerMove.cs
@@ -8,7 +8,7 @@ public class PlayerMove : MonoBehaviour
     public float anglesPerSec;
     public float db;
     public float minDb;
-    public float speed;
+    public float forceMultiplier;
     private Rigidbody rb;
     public float boostCD= 1f;
     public float boostCDRemaining;
@@ -27,34 +27,31 @@ public class PlayerMove : MonoBehaviour
     {
         db = GetComponent<AudioAnalyzer>().dbValue;
         db = Mathf.Clamp(db, 0, 100f);
-        boostCDRemaining -= Time.deltaTime;
+       
         Rotate();
         soundCircle.transform.localScale = new Vector3(0.5f*db,0.5f* db,0.5f * db);
-        if (boostCDRemaining <= 0)
-        {
-            boostCDRemaining = 0;
-            canBoost = true;           
-            
-        }
+        
 
-        if (canBoost && db > minDb)
+        if (db > minDb)
         {
             Boost();
-            boostCDRemaining = boostCD;
-            canBoost = false;
+            
         }
     }
 
     void Rotate()
     {
-        float rotSpeed = anglesPerSec * Time.deltaTime;
-        Vector3 rotation = new Vector3(0, 1 * rotSpeed, 0);
-        transform.Rotate(rotation, Space.World);
+        float rotX = Input.GetAxis("Horizontal");
+            float rotSpeed = anglesPerSec * Time.deltaTime;
+            Vector3 rotation = new Vector3(0, rotX * rotSpeed, 0);
+            transform.Rotate(rotation, Space.World);
+        
     }
 
     void Boost()
     {
-       rb.AddForce(transform.forward * db * speed * Time.deltaTime,ForceMode.Impulse);
-              
+       rb.AddForce(transform.forward * db * forceMultiplier);        
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity,5);
+        print(rb.velocity.magnitude);
     }
 }
