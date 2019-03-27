c8a2bbd8bdf831d9f2d31830e4f750e6adfb0c42
ZabaMan <47353137+ZabaMan@users.noreply.github.com>
2019-03-27 17:30:34 +0000

    Cactus Stick Behaviour

---
+++ Bull Smash Unity Project/Assets/CactusStick.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CactusStick : MonoBehaviour
+{
+
+    [SerializeField] float destroyAfterContact = 1;
+    private bool timeToReleaseCows;
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerMove>())
+        {
+            if (!timeToReleaseCows)
+            {
+                collision.gameObject.GetComponent<PlayerMove>().enabled = false;
+            }
+            Invoke("ReleaseCows", destroyAfterContact);
+
+            if (timeToReleaseCows)
+            {
+                collision.gameObject.GetComponent<PlayerMove>().enabled = true;
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerMove>())
+        {
+            collision.gameObject.GetComponent<PlayerMove>().enabled = true;
+        }
+    }
+
+    private void ReleaseCows()
+    {
+        timeToReleaseCows = true;
+        Destroy(gameObject, destroyAfterContact+ 0.1f);
+    }
+
+
+}
+
+
