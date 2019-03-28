64007159a569395ccbe3022b7b98b691d5c83ba9
ZabaMan <47353137+ZabaMan@users.noreply.github.com>
2019-03-28 17:06:01 +0000

    Started Whale

---
+++ Bull Smash Unity Project/Assets/WhaleRiseAndSpray.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhaleRiseAndSpray : MonoBehaviour
+{
+    [SerializeField] private Animator whaleAnimations;
+    [SerializeField] private float startDelay, timeTillFirstSpray, timeBetweenSprays, timeOfSprayLength, timeBetweenRises;
+    [SerializeField] private int numberOfSpraysPerRise;
+    private int spraysDone = 0;
+    [SerializeField] private ParticleSystem[] particleEffects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("Rise", startDelay);
+    }
+
+    private void Rise()
+    {
+        whaleAnimations.SetTrigger("RiseWhale");
+        Invoke("Spray", timeTillFirstSpray);
+    }
+
+    private void Spray()
+    {
+        whaleAnimations.SetBool("SprayWater", true);
+        Invoke("LowerSpray", timeOfSprayLength);
+    }
+
+    private void LowerSpray()
+    {
+        whaleAnimations.SetBool("SprayWater", false);
+        spraysDone++;
+        if (spraysDone != numberOfSpraysPerRise)
+        {
+            Invoke("Spray", timeBetweenSprays);
+        }
+        else
+        {
+            Invoke("Lower", timeTillFirstSpray);
+        }
+    }
+
+    private void Lower()
+    {
+        whaleAnimations.SetTrigger("LowerWhale");
+        Invoke("Rise", timeBetweenRises);
+    }
+}
